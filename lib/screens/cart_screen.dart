f15cfa7445f9e9cf3904a71117fa54e22e470380
venkateswarlugupta31 <31venks@gmail.com>
2021-04-13 10:12:37 +0530

    Shopping Cart - Adding to the cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,7 +2,8 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/providers/cart.dart';
+import 'package:shop/providers/cart.dart' show Cart;
+import 'package:shop/widgets/cart_item.dart' as ci;
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -10,47 +11,62 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Your Cart"),
-        ),
-        body: Column(
-          children: [
-            Card(
-              margin: EdgeInsets.all(15),
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Total",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Spacer(),
-                    Chip(
-                      label: Text(
-                        '\$${cart.totalAmount}',
-                        style: TextStyle(
-                          color: Theme.of(context)
-                              .primaryTextTheme
-                              .headline6
-                              .color,
-                        ),
+      appBar: AppBar(
+        title: Text("Your Cart"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6.color,
                       ),
-                      backgroundColor: Theme.of(context).primaryColor,
                     ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    TextButton(
-                      onPressed: () => {},
-                      child: Text('ORDER NOW'),
-                    ),
-                  ],
-                ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  TextButton(
+                    onPressed: () => {},
+                    child: Text('ORDER NOW'),
+                  ),
+                ],
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => ci.CartItem(
+                //cart.items[i].id,
+                //items is a map, need to convert to list
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].title,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
