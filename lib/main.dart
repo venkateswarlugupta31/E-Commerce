121b0bdd8b10692a6c9f0e6448dd5ba7b98c6d22
venkateswarlugupta31 <31venks@gmail.com>
2021-04-22 22:12:34 +0530

    Final Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './screens/splash_screen.dart';
 
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -55,7 +56,16 @@ class MyApp extends StatelessWidget {
             fontFamily: "Lato",
           ),
           // home: ProductsOverviewScreen(),
-          home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+          home: auth.isAuth
+              ? ProductsOverviewScreen()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
